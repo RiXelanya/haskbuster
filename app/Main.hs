d1270dbc19a79644f529a2ee2ecd00b9abb2c5ec
RXRD <lilixinxin@protonmail.com>
2023-06-29 18:20:11 +0700

    add option parser

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,40 @@
-module Haskbuster (main) where
+module Main (main) where
 
 import Lib
+import Options.Applicative
+import System.IO
+
+data Opts = Opts {
+    filename :: String
+    , baseurl :: String
+}
+
+sample :: Parser Opts
+sample = Opts
+      <$> strOption
+          ( long "file"
+         <> short 'f'
+         <> metavar "FILENAME"
+         <> help "Filename containing the wordlists" )
+      <*> strOption
+          ( long "base"
+         <> short 'b'
+         <> metavar "BASE"
+         <> help "Base url" )
+
+execBuster :: Opts -> IO ()
+execBuster (Opts file base) = do 
+    handle <- openFile file ReadMode
+    contents <- hGetContents handle
+    let dirs = words contents
+    runBuster base dirs
+execBuster _ = putStrLn "Application cant run"
 
 main :: IO ()
-main = let base = "http://en.wikipedia.org/wiki/"
-           dirs = ["Orange" , "matusa"]
-           in runBuster base dirs
+main = execBuster =<< execParser options
+    where 
+        options = info (sample <**> helper)
+          ( fullDesc
+         <> progDesc "Enumerates web directory"
+         <> header "hello - an app to enumerate web directory" )
     
